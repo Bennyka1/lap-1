4e98bbb3aebe5df5686981df81d71f3def3424c5
Bennyka1 <benjaminkampka@web.de>
2018-09-26 15:58:05 +0200

    first commit

---
+++ publish.js
@@ -0,0 +1,52 @@
+var zipFolder = require('zip-folder');
+var path = require('path');
+var fs = require('fs');
+var request = require('request');
+
+var rootFolder = path.resolve('.');
+var zipPath = path.resolve(rootFolder, '../lap-12.zip');
+var kuduApi = 'https://lap-12.scm.azurewebsites.net/api/zip/site/wwwroot';
+var userName = '$lap-12';
+var password = 'ZCfvsvAfMHW4hQoJiAaavKLPyNhr98jem457y69Ar28a6lvBtD4oDbHsD9Ti';
+
+function uploadZip(callback) {
+  fs.createReadStream(zipPath).pipe(request.put(kuduApi, {
+    auth: {
+      username: userName,
+      password: password,
+      sendImmediately: true
+    },
+    headers: {
+      "Content-Type": "applicaton/zip"
+    }
+  }))
+  .on('response', function(resp){
+    if (resp.statusCode >= 200 && resp.statusCode < 300) {
+      fs.unlink(zipPath);
+      callback(null);
+    } else if (resp.statusCode >= 400) {
+      callback(resp);
+    }
+  })
+  .on('error', function(err) {
+    callback(err)
+  });
+}
+
+function publish(callback) {
+  zipFolder(rootFolder, zipPath, function(err) {
+    if (!err) {
+      uploadZip(callback);
+    } else {
+      callback(err);
+    }
+  })
+}
+
+publish(function(err) {
+  if (!err) {
+    console.log('lap-12 publish');
+  } else {
+    console.error('failed to publish lap-12', err);
+  }
+});
